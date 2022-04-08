f6fc3862fae5f8bb7e2206ef14613b7d52f111dd
Zoltan <zoltee@gmail.com>
2022-04-08 15:36:45 -0700

    change to class

--- rethink-main.js
+++ rethink-main.js
@@ -1,135 +1,140 @@
-var BCUser;
-var BCUserAttributes;
-var brainCloudClient = new BrainCloudWrapper("_mainWrapper");
-brainCloudClient.initialize(BCAppId, BCSecret, BCVersion);
-
-const LSPrefix = 'Rethink.';
-brainCloudClient.brainCloudClient.enableLogging(true);
-function isUserLoggedIn() {
-	const userData = localStorage.getItem(LSPrefix+"BC-User");
-	const profileId = localStorage.getItem('_mainWrapper.profileId');
-	if (userData &&  profileId) {
-		if (!brainCloudClient.brainCloudClient.isAuthenticated()){
-			brainCloudClient.reconnect(); //async
-		}else{
-			return true;
-		}
+class BCUser{
+	brainCloudClient;
+	user;
+	BCUserAttributes;
+	static LSPrefix = 'Rethink.';
+	constructor(BCAppId, BCSecret, BCVersion) {
+		this.brainCloudClient = new BrainCloudWrapper("_mainWrapper");
+		this.brainCloudClient.initialize(BCAppId, BCSecret, BCVersion);
+		this.brainCloudClient.brainCloudClient.enableLogging(true);
 	}
-	return false;
-}
-function showError(message) {
-	$("#error-message").show().text(message);
-}
-function setUser(data) {
-	localStorage.setItem(LSPrefix+"BC-User", JSON.stringify(data));
-	BCUser = data;
-}
-
-function readUser() {
-	if (BCUser) {
-		return new Promise((resolve, reject) => {
-			resolve(BCUser);
-		});
-	} else {
-		return reconnectUser();
-	}
-}
-function readLS(field){
-	return localStorage.getItem(LSPrefix+field);
-}
-function writeLS(field, value){
-	localStorage.setItem(LSPrefix+field, value);
-}
-function reconnectUser() {
-	return new Promise((resolve, reject) => {
-		brainCloudClient.reconnect(function (result) {
-			if (result.status === 200) {
-				setUser(result.data);
-				resolve(result.data);
-			} else {
-				reject("Unable to load User data - "+ result.status_message);
+	isUserLoggedIn() {
+		const userData = localStorage.getItem(BCUser.LSPrefix+"BC-User");
+		const profileId = localStorage.getItem('_mainWrapper.profileId');
+		if (userData &&  profileId) {
+			if (!this.brainCloudClient.brainCloudClient.isAuthenticated()){
+				this.brainCloudClient.reconnect(); //async
+			}else{
+				return true;
 			}
-		});
-	});
-}
+		}
+		return false;
+	}
+	showError(message) {
+		$("#error-message").show().text(message);
+	}
+	setUser(data) {
+		localStorage.setItem(BCUser.LSPrefix+"BC-User", JSON.stringify(data));
+		this.user = data;
+	}
 
-function loginUser(email, password, create = false) {
-	return new Promise((resolve, reject) => {
-		if (isUserLoggedIn()) {
-			resolve(true);
+	readUser() {
+		if (this.user) {
+			return new Promise((resolve, reject) => {
+				resolve(this.user);
+			});
 		} else {
-			BCUser = null;
-			brainCloudClient.authenticateEmailPassword(
-				email,
-				password,
-				create,
-				(result) => {
-					console.log(result);
-					if (result.status === 200) {
-						setUser(result.data);
-						console.log("logged in");
-						resolve(BCUser);
-					} else {
-						reject("The email/password you entered was incorrect - " + result.status_message);
-					}
+			return this.reconnectUser();
+		}
+	}
+	readLS(field){
+		return localStorage.getItem(BCUser.LSPrefix+field);
+	}
+	writeLS(field, value){
+		localStorage.setItem(BCUser.LSPrefix+field, value);
+	}
+	reconnectUser() {
+		return new Promise((resolve, reject) => {
+			this.brainCloudClient.reconnect(result => {
+				if (result.status === 200) {
+					this.setUser(result.data);
+					resolve(result.data);
+				} else {
+					reject("Unable to load User data - "+ result.status_message);
 				}
-			);
-		}
-	});
-}
-function updateAttributes(attributes){
-	return new Promise((resolve, reject) => {
-		brainCloudClient.playerState.updateAttributes(attributes, false, result =>{
-			if (result.status === 200) {
-				resolve();
-			}else{
-				reject(result.status+' : '+ result.status_message);
-			}
+			});
 		});
-	});
-}
+	}
 
-function readAttribute(attribute){
-	return new Promise((resolve, reject) => {
-		if (BCUserAttributes && BCUserAttributes[attribute]){
-			return BCUserAttributes[attribute];
-		}
-		brainCloudClient.playerState.getAttributes(result =>{
-			if (result.status === 200) {
-				BCUserAttributes = result.data.attributes;
-				resolve(BCUserAttributes[attribute]);
-			}else{
-				reject(result.status+' : '+ result.status_message);
-			}
-		});
-	});
-}
-
-function updateUsername(username){
-	return updateAttributes({username});
-}
-
-
-
-function logout() {
-	localStorage.removeItem(LSPrefix+"BC-User");
-	return new Promise((resolve, reject) => {
-		brainCloudClient.playerState.logout((result) => {
-			resolve(true);
-		});
-	});
-}
-
-
-function readUserData(){
-	return new Promise((resolve, reject) => {
-		brainCloudClient.playerState.readUserState((result) => {
-			if (result.status === 200) {
-				setUser(result.data);
-				resolve(result.data);
+	loginUser(email, password, create = false) {
+		return new Promise((resolve, reject) => {
+			if (this.isUserLoggedIn()) {
+				resolve(true);
 			} else {
-				reject("Unable to load User data - "+ result.status_message);
+				this.user = null;
+				this.brainCloudClient.authenticateEmailPassword(
+					email,
+					password,
+					create,
+					result => {
+						console.log(result);
+						if (result.status === 200) {
+							this.setUser(result.data);
+							console.log("logged in");
+							resolve(this.user);
+						} else {
+							reject("The email/password you entered was incorrect - " + result.status_message);
+						}
+					}
+				);
 			}
 		});
-	});
-}
+	}
+	updateAttributes(attributes){
+		return new Promise((resolve, reject) => {
+			this.brainCloudClient.playerState.updateAttributes(attributes, false, result => {
+				if (result.status === 200) {
+					resolve();
+				}else{
+					reject(result.status+' : '+ result.status_message);
+				}
+			});
+		});
+	}
+
+	readAttribute(attribute){
+		return new Promise((resolve, reject) => {
+			if (this.BCUserAttributes && this.BCUserAttributes[attribute]){
+				return this.BCUserAttributes[attribute];
+			}
+			this.brainCloudClient.playerState.getAttributes(result =>{
+				if (result.status === 200) {
+					this.BCUserAttributes = result.data.attributes;
+					resolve(this.BCUserAttributes[attribute]);
+				}else{
+					reject(result.status+' : '+ result.status_message);
+				}
+			});
+		});
+	}
+
+	updateUsername(username){
+		return this.updateAttributes({username});
+	}
+
+
+
+	logout() {
+		localStorage.removeItem(BCUser.LSPrefix+"BC-User");
+		return new Promise((resolve, reject) => {
+			this.brainCloudClient.playerState.logout(result => {
+				console.log('logout', result);
+				resolve(true);
+			});
+		});
+	}
+
+
+	readUserData(){
+		return new Promise((resolve, reject) => {
+			this.brainCloudClient.playerState.readUserState(result => {
+				if (result.status === 200) {
+					this.setUser(result.data);
+					resolve(result.data);
+				} else {
+					reject("Unable to load User data - "+ result.status_message);
+				}
+			});
+		});
+	}}
+
