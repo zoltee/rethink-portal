72fb1e92e1ec51e4819b524c2a037539049ede53
Zoltan <zoltee@gmail.com>
2022-04-13 14:46:38 -0700

    new email regex

--- pages.js
+++ pages.js
@@ -42,7 +42,7 @@ class Page{
     validateEmail(emailInput){
         const email = emailInput.val();
         // if (/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(email)){
-        if (/^([!#-'*+/-9=?A-Z^-~-]+(\.[!#-'*+/-9=?A-Z^-~-]+)*|\"\(\[\]!#-[^-~ \t]|(\\[\t -~]))+\")@([!#-'*+/-9=?A-Z^-~-]+(\.[!#-'*+/-9=?A-Z^-~-]+)*|\[[\t -Z^-~]*])$/.test(email)){
+        if (/^(?:[a-z0-9!#$%&'*+\/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+\/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])$/.test(email)){
             this.bcUser.writeLS('email', email);
             return true;
         }
@@ -362,7 +362,7 @@ class ProfilePage extends Page{
                             this.bcUser.showError('Invalid email address');
                             return false;
                         }
-                        this.bcUser.user.updateEmail($inputField.val()).then(()=>{
+                        this.bcUser.updateEmail($inputField.val()).then(()=>{
                             this.bcUser.showSuccess('Email address updated');
                             $inputField.data('prev-val', $inputField.val());
                             disableEditing($inputField);
