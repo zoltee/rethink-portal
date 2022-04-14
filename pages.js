f8bd7b205e8ae4b2550e73080bf603105032d035
Zoltan <zoltee@gmail.com>
2022-04-14 16:29:05 -0700

    avatar customizer

--- pages.js
+++ pages.js
@@ -467,42 +467,44 @@ class AvatarPage extends Page{
         });
     }
     subscribe(event) {
-            const json = this.parse(event);
-
-            if (json?.source !== 'readyplayerme') {
-                return;
-            }
-
-            // Subscribe to all events sent from Ready Player Me once frame is ready
-            if (json.eventName === 'v1.frame.ready') {
-                frame.contentWindow.postMessage(
-                    JSON.stringify({
-                        target: 'readyplayerme',
-                        type: 'subscribe',
-                        eventName: 'v1.**'
-                    }),
-                    '*'
-                );
-            }
-
-            // Get avatar GLB URL
-            if (json.eventName === 'v1.avatar.exported') {
-                console.log(`Avatar URL: ${json.data.url}`);
-                // document.getElementById('avatarUrl').innerHTML = `Avatar URL: ${json.data.url}`;
-                // document.getElementById('frame').hidden = true;
-            }
-
-            // Get user id
-            if (json.eventName === 'v1.user.set') {
-                console.log(`User with id ${json.data.id} set: ${JSON.stringify(json)}`);
-            }
-        }
-    parse(event) {
+        console.log('received event', event);
+        function parse(event) {
             try {
                 return JSON.parse(event.data);
             } catch (error) {
                 return null;
             }
         }
+        const json = parse(event);
+
+        if (json?.source !== 'readyplayerme') {
+            return;
+        }
+
+        // Subscribe to all events sent from Ready Player Me once frame is ready
+        if (json.eventName === 'v1.frame.ready') {
+            frame.contentWindow.postMessage(
+                JSON.stringify({
+                    target: 'readyplayerme',
+                    type: 'subscribe',
+                    eventName: 'v1.**'
+                }),
+                '*'
+            );
+        }
+
+        // Get avatar GLB URL
+        if (json.eventName === 'v1.avatar.exported') {
+            console.log(`Avatar URL: ${json.data.url}`);
+            // document.getElementById('avatarUrl').innerHTML = `Avatar URL: ${json.data.url}`;
+            // document.getElementById('frame').hidden = true;
+        }
+
+        // Get user id
+        if (json.eventName === 'v1.user.set') {
+            console.log(`User with id ${json.data.id} set: ${JSON.stringify(json)}`);
+        }
+    }
+
 
 }
