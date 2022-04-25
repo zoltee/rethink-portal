b1d04102619cee5c33708809c42855ba37045377
Zoltan <zoltee@gmail.com>
2022-04-25 13:51:24 -0700

    avatar setting

--- pages.js
+++ pages.js
@@ -456,7 +456,7 @@ class AvatarPage extends Page{
         sampleAvatars.each((index, element) => {
             // avatarURLs.push(element.src);
             if (bcUser.userData.pictureUrl === element.src){
-                element.classList.add("selected");
+                $(element).addClass("selected");
             }
         });
         $swiperWrapper.on('click', '.swiper-slide',event => {
