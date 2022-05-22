8ca6ee6d6c9bd9d118d7db00af4c27e0cfcec645
Zoltan <zoltee@gmail.com>
2022-05-22 01:51:59 -0700

    handle email not verified error

--- rethink-main.js
+++ rethink-main.js
@@ -198,7 +198,7 @@ class BCUser{
 	async resendEmailVerification(){
 		console.log('resend verification email');
 		const response = await $.get(
-			`https://portal.braincloudservers.com/webhook/13623/resendVerMail/8496c98e-d255-4538-b072-e3af2f9e6209?email=${this.user.emailAddress}`);
+			`https://portal.braincloudservers.com/webhook/13623/resendVerMail/8496c98e-d255-4538-b072-e3af2f9e6209?email=${Utils.readLS('email')}`);
 		console.log('response', response);
 		return response?.existence ?? false;
 	}
